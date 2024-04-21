0b1915f1e17887498e545c35f26484a57d21f4d0
Harsh Agnihotri <harshagnihotri90@gmail.com>
2024-04-21 23:15:06 +0530

    api going good

---
+++ part2/fullCGI.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Content type:text/html \n\n";
+    cout << "<html>";
+    cout << "<head><title>stupid webpage</title><head>";
+    cout << "<body>";
+    cout << "<h1>this works</h1>";
+    cout << "<h1>here is your website</h1>";
+    cout << "<p>i am in love with the fairytale</p>";
+    cout << "</body>";
+    cout << "</html>";
+}
